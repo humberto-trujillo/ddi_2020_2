151938f8214951e42bc314d0692a5239bca733f5
Trujillo, Humberto <humberto.trujillo@uabc.edu.mx>
2020-09-25 19:01:32 -0700

    Coding problem

---
+++ Practicas/Assets/Scripts/CodingProblems.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CodingProblems : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int[] nums = {1,2,3,4};
+        SmallerNumbers(nums);
+    }
+
+    public int[] SmallerNumbers(int[] nums)
+    {
+        Debug.Log("Solving problem");
+        return nums;
+    }
+}
