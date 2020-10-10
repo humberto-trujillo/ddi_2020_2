8017656945020c23ae80b0cf5145838d42ee9c8c
Trujillo, Humberto <humberto.trujillo@uabc.edu.mx>
2020-10-09 19:37:07 -0700

    Interactable

---
+++ Practicas/Assets/Scripts/Interactable.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    bool isInsideZone = false;
+
+    public virtual void Interact()
+    {
+        Debug.Log("Ejecutando interacción...");
+    }
+
+    void Update()
+    {
+        if(isInsideZone && Input.GetKeyDown(KeyCode.I))
+        {
+            Interact();
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Player"))
+        {
+            return;
+        }
+        isInsideZone = true;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (!other.CompareTag("Player"))
+        {
+            return;
+        }
+        isInsideZone = false;
+    }
+}
---
+++ Practicas/Assets/Scripts/SoccerBall.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoccerBall : Interactable
+{
+    Rigidbody rb;
+    public Vector3 kickDirection;
+    public float kickForce = 30f;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public override void Interact()
+    {
+        base.Interact();
+        Debug.Log("Pateando pelota...");
+        if (rb != null)
+        {
+            rb.AddForce(kickDirection * kickForce, ForceMode.Force);
+        }
+    }
+}
