a038ce94421e9c7d7c8a5d042f88b145397d7148
Trujillo, Humberto <humberto.trujillo@uabc.edu.mx>
2020-10-23 19:27:41 -0700

    Inventory

---
+++ Practicas/Assets/Scripts/PickUp.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : Interactable
+{
+    private Inventory inventory;
+    public Item item;
+    void Start()
+    {
+        inventory = FindObjectOfType<Inventory>();
+        if (inventory == null)
+        {
+            Debug.LogWarning("No se encontró el inventario");
+        }
+    }
+
+    public override void Interact()
+    {
+        Debug.Log("Levantando item");
+        inventory.Add(item);
+        Destroy(gameObject);
+    }
+}
